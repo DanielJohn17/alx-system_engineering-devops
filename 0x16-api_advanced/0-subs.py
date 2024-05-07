5c27f8ed017191d392f869e1cadacef25a0607a5
DanielJohn17 <danielyohannes38@gmail.com>
2024-05-07 13:12:41 +0300

    Created api advanced

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ Returns number of subscribers from a subreddit"""
+    u_agent = 'Mozilla/5.0'
+
+    headers = {
+        "User-Agent": u_agent
+    }
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    res_json = response.json()
+
+    if response.status_code != 200:
+        return 0
+
+    if 'data' not in res_json:
+        return 0
+
+    if 'subscribers' not in res_json.get('data'):
+        return 0
+
+    data = res_json.get('data')
+
+    subscribers = data['subscribers']
+
+    return subscribers
